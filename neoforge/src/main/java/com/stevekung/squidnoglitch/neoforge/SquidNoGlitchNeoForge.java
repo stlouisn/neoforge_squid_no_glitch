b43602a8eebe8a069729eef017209b37340d5fc5
SteveKunG <mccommander_minecraft@hotmail.com>
2024-05-14 15:39:37 +0700

    Update to 1.20.4 + NeoForge (untested)

---
+++ neoforge/src/main/java/com/stevekung/squidnoglitch/neoforge/SquidNoGlitchNeoForge.java
@@ -0,0 +1,13 @@
+package com.stevekung.squidnoglitch.neoforge;
+
+import com.stevekung.squidnoglitch.SquidNoGlitch;
+import net.neoforged.fml.common.Mod;
+
+@Mod("squidnoglitch")
+public class SquidNoGlitchNeoForge
+{
+    static
+    {
+        SquidNoGlitch.init();
+    }
+}
